7efaf90e08b798233bc19f7aab4bd6ba0bae738f
thatonedude1982 <37455005+thatonedude1982@users.noreply.github.com>
2018-04-02 17:42:05 -0500

    Update calamity.py

--- calamity.py
+++ calamity.py
@@ -1,1 +1,31 @@
+from player import Player as pl
+
+class CalamityCard(object):
+    
+    def __init__(self):
+        self.name = 'name'
+        self.image = ''
+
+    #def clearCalamity():
+        #if town is played
+
+        #elif specific supplies are played
+
+    def brokenTongue(self):
+        self.name = 'Broken Tongue'
+
+    def snakeBite(self):
+        pl.killPlayer()
+        self.name = 'Snake Bite'
+
+    #def thief():
+        #remove a supply card from the player
+
+    #def brokenArm():
+        #player loses 2 turns
+
+    def dysentery(self, playerName):
+        self.name = 'Dysentery'
+        pl.getName(playerName)
+        pl.killPlayer()
 
