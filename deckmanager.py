32e6198c176398bb6b0472bd4c5f5f6de19cc6b5
thatonedude1982 <37455005+thatonedude1982@users.noreply.github.com>
2018-04-03 09:33:25 -0500

    Update deckmanager.py

--- deckmanager.py
+++ deckmanager.py
@@ -2,18 +2,10 @@
 import itertools, random
 from player import Player as pl
 
-i = 0 # establishing global var to iterate through shuffled deck. 
-# need to find a way to store this number so each card is not drawn
-# from the top of the deck. Also, need to figure out how to treat
-# each card as unique.
+globcounter = 0
 supplyDeck = list(itertools.product(range(26)))
-list2 = list(itertools.product(range(26)))
-inventory = list()
-#shuffle
-'''
-def randomize():   # shuffles the supply deck
-    random.shuffle(supplyDeck)
-'''
+inventory = list(itertools.product(range(8)))
+
 #populating supply list
 supplyDeck[0] = "100 Bullets"
 supplyDeck[1] = "100 Bullets"
@@ -42,62 +34,47 @@ supplyDeck[23] = "Medicine"
 supplyDeck[24] = "Medicine"
 supplyDeck[25] = "Medicine"
 
-list2[0] = "100 Bullets"
-list2[1] = "100 Bullets"
-list2[2] = "Clothes"
-list2[3] = "Clothes"
-list2[4] = "Clothes"
-list2[5] = "Oxen"
-list2[6] = "Oxen"
-list2[7] = "Oxen"
-list2[8] = "Clean Water"
-list2[9] = "Clean Water"
-list2[10] = "Clean Water"
-list2[11] = "Clean Water"
-list2[12] = "Clean Water"
-list2[13] = "200 Lbs of Food"
-list2[14] = "200 Lbs of Food"
-list2[15] = "200 Lbs of Food"
-list2[16] = "Spare Parts"
-list2[17] = "Spare Parts"
-list2[18] = "Spare Parts"
-list2[19] = "Spare Parts"
-list2[20] = "Medicine"
-list2[21] = "Medicine"
-list2[22] = "Medicine"
-list2[23] = "Medicine"
-list2[24] = "Medicine"
-list2[25] = "Medicine"
-
-def initDraw(pl):
-   print("You got: ")
-   while len(inventory) < 5:
-       rando = random.randint(0,26)
-       if supplyDeck[rando] == list2[rando]:
-           inventory.append([supplyDeck[rando]])
-           del(supplyDeck[rando])
-       else:
-           return
-       print(inventory)
-''' Loop needs to be iterated through all four players
-        so that all players get a full hand of supply cards.
-        That code will go here. At this point, we need to start 
-        merging the other pieces of code so we can run it from
-        "the top"
-'''
-def draw(pl):
+#shuffle
+def randomize():   
+    random.shuffle(supplyDeck)
+    
+def initDraw(pl): #this handles the initial supply deck each player receives.
     print("You got: ")
-    print(supplyDeck[random.randrange(0,25)])
+    global globcounter
+    for i in range (0,4):
+        while globcounter < 25: #handles iterating through entire deck while making it "end of deck aware"
+            pl.inventory = (supplyDeck[globcounter])
+            globcounter += 1
+            print pl.inventory
+            #inventory.append(supplyDeck[globcounter])
+            #code to iterate loop to next player
+            break
+    else: # if out of cards, reshuffle and start at the top.
+        randomize()
+        globcounter = 0
+      
+def draw(pl):
+    global globcounter
+    for i in range(1): #almost the same as the initDraw function, but one card at a time.
+        while globcounter < 25:
+            print("You got: ")
+            inventory.append(supplyDeck[globcounter])
+            print(supplyDeck[globcounter])
+            globcounter += 1
+            break
+        else:
+            randomize()
+            globcounter = 0
 
-def discard():
+def discard():#eventually writing a discard function 
     print ("test")    
 
-def buy():
+def buy():#eventually a function to trade two of the same cards for one supply of players choice
     print("test")
 
-#randomize() # this print is just in to test shuffle function
-#print (supplyDeck)
+randomize() #calling shuffle function for testing
 
-initDraw(pl) # this is for first draw of game where players get 5 cards
+initDraw(pl) # this is for first draw of game where players get 5 cards calling for testing
+print (pl.inventory) # printing pl.inventory to ensure player inventory is being updated.
 
-draw(pl)# this is for any subsequent draws made by players.
+draw(pl)# this is for any subsequent draws made by players calling for testing
